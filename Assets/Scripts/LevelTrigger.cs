783abbb2890376fdb7487a2e926ecdcb246675e7
Nam Tran <tranngocnam97@yahoo.com>
2015-02-03 23:38:27 -0500

    Cleaned up unnecessary files

---
+++ Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTrigger : MonoBehaviour {
+
+	public GameObject target;
+	public int level;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log (other.gameObject.name);
+		if (other.tag == "Player") {
+			Application.LoadLevel(level);
+		}
+	}
+}
---
+++ Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallaxing : MonoBehaviour {
+	
+	public Transform[] layers;				// Array of layers to be parallaxed
+	private float[] parallaxScales;			// Scaling factors of the camera movement to move each layer
+	
+	public float parallaxingAmount = 1f;	// How much to smooth the parallaxing (Cannout be 0.0)
+	
+	private Transform camera;				// Main Camera reference
+	private Vector3 previousCameraPosition;	// The position of the camera in the previous frame
+
+	// Called before Start()
+	void Awake () {
+		camera = Camera.main.transform;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		previousCameraPosition = camera.position;		// Sets the previous camera position to the current one
+		
+		parallaxScales = new float[layers.Length];		// Instantiates the parallax scale array with as many elements as layers
+		for (int i = 0; i < layers.Length; i++) {
+			parallaxScales[i] = layers[i].position.z * -1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < layers.Length; i++) {
+			float parallax = (previousCameraPosition.x - camera.position.x) * parallaxScales[i];
+			float layerTargetPositionX = layers[i].position.x + parallax;
+			Vector3 layerTargetPosition = new Vector3(layerTargetPositionX, layers[i].position.y, layers[i].position.z);
+			layers[i].position = Vector3.Lerp (layers[i].position, layerTargetPosition, parallaxingAmount * Time.deltaTime);
+		}
+		
+		previousCameraPosition = camera.position;
+	}
+	
+	
+}
